5109a135012317aa1e4db17a4fe846fdf9d8993a
Nnanyielugo <kz.onyegbule@gmail.com>
2019-09-09 07:12:41 +0100

    move allowed child types to separate file
    add toggle component to allowed child types

---
+++ src/allowed-child-types.js
@@ -0,0 +1,19 @@
+const childTypes = [
+  'checkbox',
+  'container',
+  'link',
+  'image',
+  'input',
+  'form',
+  'search',
+  'text',
+  'loader',
+  'radio',
+  'map',
+  'nav',
+  'video',
+  'dropdown',
+  'toggle'
+];
+
+export default childTypes;
